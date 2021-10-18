722be256f3ab9a65b4e0127492585096cf53744a
Nguyễn Công Cương <cuongnc.min@outlook.com>
2021-10-18 21:28:36 +0700

    Firebase hosting config

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GlobalStyles } from "./global-styles";
-import { FirebaseContext } from "./context/firebase";
 
+import { FirebaseContext } from "./context/firebase";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvxxIGRy0NtVQ1gEq_ah1f6-5xhYqEsa0",
+  authDomain: "netflix-clone-7c192.firebaseapp.com",
+  projectId: "netflix-clone-7c192",
+  storageBucket: "netflix-clone-7c192.appspot.com",
+  messagingSenderId: "803918221693",
+  appId: "1:803918221693:web:39227e13d63dd4b37d3c14"
+};
+
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
-    <FirebaseContext.Provider>
+    <FirebaseContext.Provider value={{ firebase: app }}>
       <GlobalStyles />
       <App />
     </FirebaseContext.Provider>
